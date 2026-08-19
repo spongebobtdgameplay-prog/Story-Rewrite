53eee2e07a0fa957300e7224fe3d84229b346d37
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-19 18:34:21 -0400

    Require moderated multiplayer backend version

---
+++ moderation-version.js
@@ -0,0 +1,16 @@
+const REQUIRED_MODERATION_SERVER_VERSION = 9;
+
+if (typeof EnsureBackendVersion === "function") {
+    const BaseModerationVersionCheck = EnsureBackendVersion;
+
+    EnsureBackendVersion = async function() {
+        const Health = await BaseModerationVersionCheck();
+        const Version = Number(Health?.version || 0);
+
+        if (Version < REQUIRED_MODERATION_SERVER_VERSION) {
+            throw new Error("MULTIPLAYER_UPDATING");
+        }
+
+        return Health;
+    };
+}
