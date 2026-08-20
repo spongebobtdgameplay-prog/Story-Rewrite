a3e66514e993ccf03555dfab729f3bdf98e91ba7
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-20 10:54:53 -0400

    Route protected pages through persistent app shell

--- server-config.js
+++ server-config.js
@@ -1,1 +1,23 @@
 window.STORY_REWRITE_SERVER_URL = "https://story-rewrite-backend.onrender.com";
+
+(() => {
+    if (window.top !== window.self) return;
+
+    const ManagedPages = new Set([
+        "main.html",
+        "levels.html",
+        "dialog.html",
+        "multiplayer.html",
+        "tutorial.html",
+        "rules.html",
+        "account.html"
+    ]);
+
+    const PageName = (window.location.pathname.split("/").pop() || "").trim();
+    if (!ManagedPages.has(PageName)) return;
+
+    const Route = `${PageName}${window.location.search}${window.location.hash}`;
+    const ShellUrl = new URL("index.html", window.location.href);
+    ShellUrl.hash = encodeURIComponent(Route);
+    window.location.replace(ShellUrl.href);
+})();
