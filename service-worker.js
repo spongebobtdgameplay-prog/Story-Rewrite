659d59d6a8f25845184cef6fcd7b0c615a682018
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-20 12:23:51 -0400

    Add one-purpose worker cleanup shim

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", Event => {
+    Event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", Event => {
+    Event.waitUntil((async () => {
+        const CacheNames = await caches.keys();
+        await Promise.all(CacheNames
+            .filter(CacheName => CacheName.startsWith("story-rewrite-"))
+            .map(CacheName => caches.delete(CacheName)));
+        await self.clients.claim();
+    })());
+});
