9a25ba8f6e6e9973f5253530ea4a19b94a8291a4
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 15:13:45 -0400

    Require persistent moderation backend v21

--- moderation-version.js
+++ moderation-version.js
@@ -1,4 +1,4 @@
-const REQUIRED_MODERATION_SERVER_VERSION = 20;
+const REQUIRED_MODERATION_SERVER_VERSION = 21;
 
 if (typeof EnsureBackendVersion === "function") {
     const BaseModerationVersionCheck = EnsureBackendVersion;
