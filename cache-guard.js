bad781ccbcf59dbf9f24d330ee70086d248ed8bc
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 19:36:17 -0400

    Bump frontend cache cleanup version

--- cache-guard.js
+++ cache-guard.js
@@ -1,5 +1,5 @@
 (() => {
-    const CleanupVersion = "20260822-72";
+    const CleanupVersion = "20260822-73";
     const ReloadedKey = `StoryRewriteCacheCleanup-${CleanupVersion}-reloaded`;
 
     async function ClearLegacyCaches() {
