d26cdd4f9014b8f1a0860a0c0bcc7742c1fa2446
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 19:36:10 -0400

    Bump frontend build to v2.26

--- build-version.js
+++ build-version.js
@@ -1,4 +1,4 @@
-const STORY_BUILD_VERSION = "v2.06";
+const STORY_BUILD_VERSION = "v2.26";
 
 function ApplyStoryBuildVersion() {
     for (const Badge of document.querySelectorAll(".StoryBuildVersion")) {
