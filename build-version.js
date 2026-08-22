793f96f6ff53a74eb1a3375f8e5153109fc18455
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 19:09:33 -0400

    Bump frontend patch version to v2.04

--- build-version.js
+++ build-version.js
@@ -1,4 +1,4 @@
-const STORY_BUILD_VERSION = "v2.03";
+const STORY_BUILD_VERSION = "v2.04";
 
 function ApplyStoryBuildVersion() {
     for (const Badge of document.querySelectorAll(".StoryBuildVersion")) {
