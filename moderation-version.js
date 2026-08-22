1bfb052c161f09e0dfeea5193927d97ed99d8f08
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 14:28:39 -0400

    Censor obfuscated words anywhere in multiplayer messages

--- moderation-version.js
+++ moderation-version.js
@@ -1,4 +1,4 @@
-const REQUIRED_MODERATION_SERVER_VERSION = 17;
+const REQUIRED_MODERATION_SERVER_VERSION = 18;
 
 if (typeof EnsureBackendVersion === "function") {
     const BaseModerationVersionCheck = EnsureBackendVersion;
