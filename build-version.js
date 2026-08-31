f4740f6f5946d7abc9557c153de954a8dc0d18db
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-30 20:50:42 -0400

    Bump Story Rewrite build to v2.39

--- build-version.js
+++ build-version.js
@@ -1,4 +1,4 @@
-const STORY_BUILD_VERSION = "v2.38";
+const STORY_BUILD_VERSION = "v2.39";
 
 function ApplyStoryBuildVersion() {
     for (const Badge of document.querySelectorAll(".StoryBuildVersion")) {
